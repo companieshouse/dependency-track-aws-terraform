efd1fc893c128276348805835a8b4023cf664bba
JoelClemence <jclemence@companieshouse.gov.uk>
2024-04-03 15:03:20 +0100

    Cluster not rand

--- groups/infrastructure/ecs.tf
+++ groups/infrastructure/ecs.tf
@@ -2,6 +2,6 @@
 * Provisions an ECS Cluster for running the Dependency Track services.
 */
 
-resource "aws_ecs_cluster" "rand" {
+resource "aws_ecs_cluster" "cluster" {
   name = "${local.name_prefix}-stack"
 }
